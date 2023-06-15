a88eb3adff5d187f561c18ff5506ab204587db42
Zerbaib <hugo.hennetin@proton.me>
2023-06-15 18:34:06 +0200

    V1 reboot

--- exemple.config.py
+++ exemple.config.py
@@ -1,5 +1,9 @@
-# Informations du bot
-TOKEN = 'YOUR_BOT_TOKEN'
-YOUR_ID = YOUR_USER_ID
-CHAN_ID = YOUR_CHANNEL_ID
+TOKEN = "YOUR BOT TOKEN"
+CHAN_ID = 000
+YOUR_ID = 000
+servers = [
+        {'name': 'Serveur 1', 'ip': '127.0.0.1', 'maintenance': False},
+        {'name': 'Serveur 2', 'ip': '192.168.0.1', 'maintenance': False},
+        {'name': 'Serveur 3', 'ip': 'example.com', 'maintenance': False},
+]
 sec_loop = 60
