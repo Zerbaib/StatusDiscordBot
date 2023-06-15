452fd8d12e987ab2adfbc6144339fed40b75f47f
Zerbaib <hugo.hennetin@proton.me>
2023-06-15 18:47:19 +0200

    finish bot

--- main.py
+++ main.py
@@ -45,16 +45,6 @@ async def update_servers_status():
 
             embed.add_field(name=name, value=status, inline=False)
 
-            if last_updated:
-                time_delta = datetime.datetime.now() - last_updated
-                embed.add_field(name="Time Since Last Update", value=str(time_delta))
-
-            if status != server['status'] or maintenance != server['maintenance']:
-                server['status'] = status
-                server['maintenance'] = maintenance
-                server['last_updated'] = datetime.datetime.now()
-                save_config()
-
         if embed_message:
             embed.add_field(name="legend", value="If is <:on:1118875860854915152> Is online !\nIf is <:idle:1118875857512038560> the server have bugs\nIf is <:off:1118875858841649183> The server is offline", inline=False)
             await embed_message.edit(embed=embed)
@@ -78,10 +68,6 @@ async def ping_server(ip):
     except Exception:
         return 'Error while pinging'
 
-def save_config():
-    with open('config.py', 'w') as config_file:
-        json.dump(config.servers, config_file)
-
 @bot.slash_command(
     name="maintenance",
     description="Activate or deactivate maintenance mode for a server"
@@ -94,12 +80,9 @@ async def maintenance(ctx: disnake.ApplicationCommandInteraction, serveur: str):
     server = next((s for s in config.servers if s['name'].lower() == serveur.lower()), None)
 
     if server:
-        server['maintenance'] = not server['maintenance']
-        server['last_updated'] = datetime.datetime.now()
-        save_config()
-        await ctx.send(f"The server {server['name']} is now in maintenance: {server['maintenance']}")
+        await ctx.author.send(f"The server {server['name']} is now in maintenance: {server['maintenance']}")
     else:
-        await ctx.send("Server not found")
+        await ctx.author.send("Server not found")
 
 bot.loop.create_task(update_servers_status())
 bot.run(config.TOKEN)
