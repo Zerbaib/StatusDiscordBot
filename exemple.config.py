be73a698e6c7db7f5bf530583564a89fa5837110
Zerbaib <hugo.hennetin@proton.me>
2023-06-23 14:06:09 +0200

    [DEL] python config and templet we don't need

--- exemple.config.py
+++
@@ -1,5 +0,0 @@
-TOKEN = 'your_bot_token'
-CHAN_ID = 1234567890  # ID of the channel where the server status will be displayed
-YOUR_ID = 1234567890  # Your user ID (to authorize maintenance command)
-sec_loop = 10  # Number of seconds between each status update loop
-del_time = 2
